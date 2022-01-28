6c2d6138bd884389bed86d1f654130c6072710c2
StarsL.cn <starsliao@163.com>
2022-01-28 17:11:03 +0800

    独立 Blackbox Manager

--- Blackbox-Manager/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-eth=$(ls /sys/class/net/ | grep -v "`ls /sys/devices/virtual/net/`")
-ip=$(ip addr | grep $eth | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}' | head -1)
-cd flask-consul
-docker build -t flask-consul:latest .
-cd ../nginx-consul
-docker build -t nginx-consul:latest .
-echo -e "\n\nBlackbox-Manager:\nhttp://${ip}:1026"
---
+++ blackbox_exporter/README.md
@@ -0,0 +1,4 @@
+### 需要配合Blackbox Manager使用
+
+### 参考：[Blackbox Manager & Blackbox Exporter Dashboard](https://github.com/starsliao/ConsulManager)
+
